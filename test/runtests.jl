1a045d34477fbd278b9fd0e8902f40722c8d8467
Jonathan Goldfarb <jgoldfar@gmail.com>
2017-11-17 13:50:43 -0500

    Start defining interface/parser for flat file format

--- test/runtests.jl
+++ test/runtests.jl
@@ -2,4 +2,13 @@ using SPSInterface
 using Base.Test
 
 # write your own tests here
-@test 1 == 1
+@testset "SPSInterface" begin
+    @testset "importFile" begin
+        testFile1 = joinpath(@__DIR__, "testSchedule1.dat")
+        osched1, employeeList1 = importFile(testFile1)
+        @test employeeList1 isa Vector{Employee}
+    end
+    @testset "exportFile" begin
+
+    end
+end
