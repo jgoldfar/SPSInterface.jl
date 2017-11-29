aa5872aa32cb11f40782fa07f4a0233e9ac725f6
Jonathan Goldfarb <jgoldfar@gmail.com>
2017-11-28 20:38:33 -0500

    Implement file import, which required finishing up main parsing loop and implementing routines to do low-level parsing. Test suite for hand-written files/situations included in this commit. exportFile implementation also added in this commit.

--- src/SPSInterface.jl
+++ src/SPSInterface.jl
@@ -7,6 +7,7 @@ export importFile, exportFile, Schedule, Employee
 _is_comment(line::AbstractString) = isempty(line) || startswith(line, '%')
 _is_secHeader(line::AbstractString) = startswith(line, '#')
 
+const match_tripleHead = r"###\s*([^#]+)"
 const match_doubleHead = r"##\s*([^#]+)"
 const match_singleHead = r"#\s*([^#]+)"
 function importFile(path::AbstractString)
@@ -14,10 +15,12 @@ function importFile(path::AbstractString)
     overallSchedule = Dict(i => Tuple{Float64, Float64}[] for i in 1:5)
     
     parsingEmployee = false
+    parsingAvailability = false
     tmpEmployeeName = ""
+    tmpEmployeeMaxHours = Inf
     tmpEmployeeAvail = SPSBase.emptySchedule(Float64)
 
-    employees = Employee[]
+    employees = Employee{Float64}[]
     currLineNumber = 0
     for line in map(strip, eachline(path))
         currLineNumber += 1
@@ -26,6 +29,12 @@ function importFile(path::AbstractString)
             if parsingEmployee
                 # Finalize employee parsing
                 parsingEmployee = false
+                if !isempty(tmpEmployeeAvail)
+                    push!(employees, Employee(String(tmpEmployeeName), tmpEmployeeAvail, tmpEmployeeMaxHours))
+                end
+                tmpEmployeeName = ""
+                tmpEmployeeMaxHours = Inf
+                tmpEmployeeAvail = SPSBase.emptySchedule(Float64)
             elseif parsingOverallSchedule
                 # Finalize overall schedule
                 parsingOverallSchedule = false
@@ -34,34 +43,133 @@ function importFile(path::AbstractString)
             # Continue parsing file
             singleHeadMatch = match(match_singleHead, line)
             doubleHeadMatch = match(match_doubleHead, line)
+            tripleHeadMatch = match(match_tripleHead, line)
+
+            # Currently no subsubsections should be parsed
+            if tripleHeadMatch != nothing
+                continue
+            end
             
-            if doubleHeadMatch != nothing
+            if parsingAvailability && doubleHeadMatch != nothing
                 # New employee
-                info("Found new employee: $(doubleHeadMatch[1])")
+                # info("Found new employee: $(doubleHeadMatch[1])")
                 parsingEmployee = true
                 tmpEmployeeName = doubleHeadMatch[1]
                 tmpEmployeeAvail = SPSBase.emptySchedule(Float64)
             elseif singleHeadMatch != nothing
                 # New section (either Overall, Availability, or other...)
-                info("Parsing new section: $(singleHeadMatch[1])")
+                # info("Parsing new section: $(singleHeadMatch[1])")
                 if singleHeadMatch[1] == "Overall"
                     parsingOverallSchedule = true
+                elseif singleHeadMatch[1] == "Availability"
+                    parsingAvailability = true
+                else
+                    parsingOverallSchedule = false
+                    parsingAvailability = false
                 end
             else
                 warn("Header not recognized on line $(currLineNumber): ", line)
             end
         elseif parsingEmployee
-            println("Employeeline: $(tmpEmployeeName): $(line)")
+            line = lowercase(line)
+            if is_schedline(line)
+                parse_SchedLine!(tmpEmployeeAvail, line)
+            elseif startswith(line, "maxhours")
+                tmpEmployeeMaxHours = min(parse(Float64, line[9:end]), tmpEmployeeMaxHours)
+            end
         elseif parsingOverallSchedule
-            println("Osched: $(line)")
-        else
-            warn("Line $(currLineNumber) not parsed: ", line)
+            line = lowercase(line)
+            if is_schedline(line)
+                parse_SchedLine!(overallSchedule, line)
+            end
         end
+    end
+    if parsingEmployee
+        # Finalize employee parsing
+        if !isempty(tmpEmployeeAvail)
+            push!(employees, Employee(String(tmpEmployeeName), tmpEmployeeAvail, tmpEmployeeMaxHours))
+        end
+        tmpEmployeeName = ""
+        tmpEmployeeMaxHours = Inf
+        tmpEmployeeAvail = SPSBase.emptySchedule(Float64)
     end
     overallSchedule, employees
 end
-function exportFile(path::AbstractString, bsl::BitScheduleList)
 
+const dayMap = Dict('m' => 1, 't' => 2, 'w' => 3, 'r' => 4, 'f' => 5)
+
+function is_schedline(line)
+    for d in keys(dayMap)
+        startswith(line, "$d ") && return true
+    end
+    return false
 end
 
+function parse_SchedLine!(sched::Schedule{T}, line) where {T}
+    times = line[3:end]
+    timesout = _parse_SchedLine(times)
+    append!(getfield(sched, dayMap[line[1]]), timesout)
+end
+function parse_SchedLine!(sched::Dict{Int, Vector{Tuple{Float64, Float64}}}, line)
+    timesout = _parse_SchedLine(line[3:end])
+    sched[dayMap[line[1]]] = timesout
+end
+function _parse_SchedLine(line)
+    map(_parse_timeSpan ∘ strip, split(line, [',']))
+end
+function _parse_timeSpan(line)
+    tuple(map(_parse_time, split(line, "-"))...)
+end
+function _parse_time(line)::Float64
+    if contains(line, ":")
+        h, m = split(line, ':')
+        parse(Int, h) + parse(Int, m)//60
+    else
+        parse(Int, line)
+    end
+end
+# function exportFile(path::AbstractString, bsl::BitScheduleList)
+#     exportFile(path, SPSBase.to_sched(bsl))
+# end
+function exportFile(path, empList::EmployeeList{T}) where {T}
+    open(path, "w") do st
+        for emp in empList
+            println(st, "## $(emp.name)")
+            if emp.maxTime != Inf
+                println(st, "MaxHours $(emp.maxTime)")
+            end
+            if emp.specialty != 0
+                println(st, "SpecialtyCode $(emp.specialty)")
+            end
+            _exportSched(st, avail(emp))
+            print(st, "\n\n")
+        end
+    end
+    return nothing
+end
+function _exportSched(st::IO, s::Schedule{T}) where {T}
+    print(st, "M ")
+    _exportSchedDay(st, s.day1)
+    print(st, "\nT ")
+    _exportSchedDay(st, s.day2)
+    print(st, "\nW ")
+    _exportSchedDay(st, s.day3)
+    print(st, "\nR ")
+    _exportSchedDay(st, s.day4)
+    print(st, "\nF ")
+    _exportSchedDay(st, s.day5)
+    return nothing
+end
+_exportSchedDay(st::IO, d) = join(st, map(_show_span, d), ", ")
+_show_span(s) = join(map(_nearest_frac_time, s), "-")
+
+function _nearest_frac_time(t)
+    h = floor(Int, t)
+    m = floor(Int, 60*(t - h))
+    if m == 0
+        @sprintf "%d" h
+    else
+        @sprintf "%d:%d" h m
+    end
+end
 end # module
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,12 +1,44 @@
 using SPSInterface
-using Base.Test
+using SPSBase
+using Compat
+
+@static if VERSION >= v"0.7-"
+    using Test
+else
+    using Base.Test
+end
 
 # write your own tests here
 @testset "SPSInterface" begin
     @testset "importFile" begin
+        # contents of testFile1 are hard-coded into this file
         testFile1 = joinpath(@__DIR__, "testSchedule1.dat")
         osched1, employeeList1 = importFile(testFile1)
-        @test employeeList1 isa Vector{Employee}
+
+        @test first(osched1[1]) == (9.0, 17.5) # M 9-17:30
+        @test first(osched1[2]) == (9.0, 17.0) # T 9-17
+        @test first(osched1[4]) == (9.0, 19.0) # R 9-19
+
+        emp1 = employeeList1[1]
+        @test isapprox(emp1.maxTime, 10) # MaxHours 10
+        @test isempty(avail(emp1, 2)) && isempty(avail(emp1, :day4))
+
+        emp2 = employeeList1[2]
+        @test isapprox(emp2.maxTime, 2)
+        @test isempty(avail(emp2, :day2)) && isempty(avail(emp2, :day4)) && isempty(avail(emp2, :day5))
+        @test length(avail(emp2, :day1)) == 1 && first(avail(emp2, :day1)) == (9.0, 10.0)
+        @test isapprox(first(avail(emp2, :day3))[2], 24+14//60)
+
+        emp3 = employeeList1[3]
+        @test isapprox(emp3.maxTime, 3)
+        @test !isempty(avail(emp3, :day4))
+        @test length(avail(emp3, :day4)) == 1 && first(avail(emp3, :day4)) == (10.0, 13.0)
+
+        testFileS = joinpath(@__DIR__, "testScheduleSimple.dat")
+        oschedS, employeeListS = importFile(testFileS)
+
+        @test length(employeeListS) == 2
+        @test all(isempty(last(t)) for t in oschedS)
     end
     @testset "exportFile" begin
 
